11078d0d895e19e2b37b258692707193dadb9b48
SERDUSSS <serdusone@gmail.com>
2024-06-17 19:28:43 +0200

    deleted:    test

--- allowlist.py
+++ allowlist.py
@@ -1,1 +1,1 @@
-AllowList = ["com.termux.api","com.termux", "com.discord", "com.whatsapp"]
+AllowList = ["com.termux.api","com.termux", "com.discord", "com.whatsapp"] # Add here all apps you want to get notifications from
